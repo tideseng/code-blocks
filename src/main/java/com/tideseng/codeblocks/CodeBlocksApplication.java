a742a88f5ece96535148fac4fcd1c6ea9ccc76db
tideseng <justdoit_onebyone@163.com>
2020-07-04 19:56:46 +0800

    init

---
+++ src/main/java/com/tideseng/codeblocks/CodeBlocksApplication.java
@@ -0,0 +1,13 @@
+package com.tideseng.codeblocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBlocksApplication.class, args);
+	}
+
+}
